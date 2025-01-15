0c4bfa139e08d60de967c28251451ee411cec810
Bianity <45706212+Bianity@users.noreply.github.com>
2025-01-16 06:11:52 +0700

    Dev Bianity New v3.1
    Dev Bianity New v3.1
    
    Signed-off-by: Bianity <45706212+Bianity@users.noreply.github.com>

--- routes/auth.php
+++ routes/auth.php
@@ -5,20 +5,21 @@ use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\SocialController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('auth/{provider}', [SocialController::class, 'redirectToProvider']);
-Route::get('auth/{provider}/callback', [SocialController::class, 'handleProviderCallback']);
-
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+    Route::get('auth/social/{provider}', [SocialController::class, 'redirectToProvider'])->name('auth.social');
+    Route::get('auth/social/{provider}/callback', [SocialController::class, 'handleProviderCallback']);
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::middleware(['registration.disable'])->group(function () {
+        Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+        Route::post('register', [RegisteredUserController::class, 'store']);
+    });
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
@@ -35,14 +36,14 @@ Route::middleware('guest')->group(function () {
         ->name('password.reset');
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.update');
+        ->name('password.store');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
+    Route::get('verify-email', EmailVerificationPromptController::class)
         ->name('verification.notice');
 
-    Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
 
@@ -55,6 +56,14 @@ Route::middleware('auth')->group(function () {
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
+    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+
+    Route::get('login/2fa', [AuthenticatedSessionController::class, 'twoFactorAuthentication'])
+        ->name('login.2fa');
+
+    Route::post('login/2fa', [AuthenticatedSessionController::class, 'twoFactorAuthenticationStore'])
+        ->name('login.2fa.store');
 });
--- routes/console.php
+++ routes/console.php
@@ -2,18 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+})->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
+
+Schedule::command('app:update-user-level')->daily();
+Schedule::command('app:update-user-membership-years-badge')->daily();
+
+Schedule::command('app:update-sitemap')->daily();
+
+Schedule::command('disposable:update')->weekly();
